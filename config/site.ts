c6281f637a226f1bf448d8a0bc65a9690297419d
Vik <macm4@AQS.local>
2025-11-25 09:20:41 -0800

    Updated Navbar

---
+++ config/site.ts
@@ -0,0 +1,73 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Infinite Tech Repair",
+  description: "Your One-Stop Shop for All Tech Repairs",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Repairs",
+      href: "/repairs",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Track Repair",
+      href: "/track-repair",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Repairs",
+      href: "/repairs",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Track Repair",
+      href: "/track-repair",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
